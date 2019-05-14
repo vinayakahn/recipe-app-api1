52856f1aa07bb09fcd5a3c35b1c4337086cacf9d
vinayakahn <vinayakavinu08@gmail.com>
2019-05-15 00:18:06 +0530

    User admin customization model creates the user page.

--- core/admin.py
+++ core/admin.py
@@ -25,4 +25,12 @@ class UserAdmin(BaseUserAdmin):
     )
 
 
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'password1', 'password2')
+        }),
+    )
+
+
 admin.site.register(models.User, UserAdmin)
